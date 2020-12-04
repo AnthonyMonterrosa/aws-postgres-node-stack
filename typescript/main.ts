414f9eb59cb4679d6def65cd5b9800005eedd727
Anthony Monterrosa <12419195+AnthonyMonterrosa@users.noreply.github.com>
2020-12-03 23:54:05 -0700

    Fix SQL connection to always close on process exit.

--- typescript/main.ts
+++ typescript/main.ts
@@ -2,32 +2,30 @@ import postgres from 'postgres';
 import express  from 'express';
 import { BuildConnectionString } from './database/utility';
 
-( async () => {
+const sql: postgres.Sql<null> = postgres(
+	BuildConnectionString(
+		{ 	
+			host: 'localhost',
+			database: 'docker-example',
+			username: 'postgres',
+			password: 'docker'
+		}
+	),
+	{  
+		connect_timeout: 30,
+		connection: 
+		{
+			application_name: 'node-express.js'
+		},
+		port: 5430
+	} 
+);
 
-	const sql: postgres.Sql<null> = postgres(
-		BuildConnectionString(
-			{ 	
-				host: 'localhost',
-				database: 'docker-example',
-				username: 'postgres',
-				password: 'docker'
-			}
-		),
-		{  
-			connect_timeout: 30,
-			connection: 
-			{
-				application_name: 'node-express.js'
-			},
-			port: 5430
-		} 
-	);
+( async () => {
 
 	const response = await sql`
 		select * from "docker table"
 	`;
-
-	await sql.end({ timeout: 3 });
 
 	response.forEach( row => console.log( row ) );
 
@@ -42,7 +40,10 @@ import { BuildConnectionString } from './database/utility';
 } )()
 	.catch( reason => 
 	{
-		console.log( 'main errored with the following exception:' );
+		console.log( 'Error: main errored with the following exception:' );
 		console.log( reason );
 		process.exit(1);
+	} )
+	.finally( () => {
+		sql.end( { timeout: 0 } );
 	} );
